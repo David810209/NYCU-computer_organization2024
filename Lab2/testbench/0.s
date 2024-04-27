9538cc1c4dd82db20f769ba22fdb8c416dff79f1
David810209 <a1050120bc@gmail.com>
2024-04-27 23:56:55 +0800

    0427_23.56

--- Lab2/testbench/0.s
+++ Lab2/testbench/0.s
@@ -1,33 +1,29 @@
         .data   0x10008000      # start of Dynamic Data (pointed by $gp)
-val1:   .word   0x00114514      # Some test data
-val2:   .word   0xf1919810
+hun:    .word   0x00114514      # 0($gp)
+hah:    .word   0xf1919810
         .word   0x1
         .word   0x2
-        .word   0x3             # Data array
+        .word   0x3             # 16($gp)
 
-        .text   0x00400000      # start of Text (program code)
-start:  lui     $t0, 0x1000     # Load upper immediate for address calculation
-        ori     $t0, $t0, 0x8000 # Form address in $t0
-
-        add     $t1, $t0, $zero # $t1 = $t0 (copy base address)
-        lw      $t2, 0($t1)     # Load word from address in $t1
-        lw      $t3, 4($t1)     # Load next word
-
-        add     $t4, $t2, $t3   # $t4 = $t2 + $t3
-        sub     $t5, $t2, $t3   # $t5 = $t2 - $t3
-        and     $t6, $t2, $t3   # $t6 = $t2 & $t3
-        or      $t7, $t2, $t3   # $t7 = $t2 | $t3
-        slt     $t8, $t2, $t3   # Set $t8 = 1 if $t2 < $t3, else 0
-
-        sw      $t4, 8($t1)     # Store result of add at address $t1 + 8
-        sw      $t5, 12($t1)    # Store result of sub at address $t1 + 12
-
-        nop                     # No operation (useful for delay slots)
-
-        beq     $t2, $t3, equal # Branch to "equal" if $t2 == $t3
-        j       end             # Jump to end if not equal
-
-equal:  li      $t9, 0xAABB     # Load immediate (pseudoinstruction)
-        j       end             # Jump to end
-
-end:    nop                     # End of the program, do nothing
+        .text   0x00400000      # start of Text (pointed by PC), 
+                                # Be careful there might be some other instructions in JsSPIM.
+                                # Recommend at least 9 instructions to cover out those other instructions.
+main:   li      $t0, 0x12345678
+        sub     $t1, $gp, $t0   
+        slt     $a0, $t1, $gp   
+        add     $t2, $t1, $gp   # $t0 = $gp
+        nop                     # test NOP
+        and     $t4, $t1, $t2   # test AND
+        or      $t5, $t1, $t2   # test OR
+        nop                     # test NOP
+        lw      $t5, hun     # test LW
+        sw      $t5, 8($gp)     # test SW
+        nop                     # test NOP
+        j       end             # [btg] should jump
+end:    lw      $t5, hah        # [end]
+        sw      $t5, 12($gp)
+        nop                     # test NOP
+        li      $a3, 0xa114514a # test li (lui, ori)
+        slt     $t6, $t2, $t3
+        beq     $zero, $t6, func
+func:   li      $t6, 0x18476502
